8d6e678da31956e516f1e8867d1b435345d60f5f
Tanish Gupta <162000530+tanishkrit@users.noreply.github.com>
2026-09-18 16:41:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Longest Word/longest-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longest(String[] arr) 
+    {
+        String ans = arr[0];
+        
+        for(int i = 1; i < arr.length; i++)
+        {
+            if(arr[i].length() > ans.length())
+            {
+                ans = arr[i];
+            }
+        }
+        
+        return ans;
+        
+    }
+}
