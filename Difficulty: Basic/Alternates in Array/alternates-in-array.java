63215d7377b9c270edbb997e60f92e0c258f5982
Tanish Gupta <162000530+tanishkrit@users.noreply.github.com>
2026-09-23 18:51:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Alternates in Array/alternates-in-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ArrayList<Integer> getAlternates(int arr[]) {
+        // Code Here
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i+=2)
+        {
+            ans.add(arr[i]);
+        }
+        return ans;
+        
+    }
+}
